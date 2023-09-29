b05d559560aa2df95749fe8a81ee561ca90f7fa4
linuxmel27 <melvinpg7@gmail.com>
2023-09-28 21:28:49 -0600

    Nuevos cambios

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -13,6 +14,23 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/guardar_datos", methods=["POST"])
+def guardar_datos():
+    campo1 = request.form["campo1"]
+    campo2 = request.form["campo2"]
+
+    # Conexión a la base de datos SQLite
+    conn = sqlite3.connect("datos.db")
+    cursor = conn.cursor()
+
+    # Insertar datos en la base de datos
+    cursor.execute('INSERT INTO datos (campo1, campo2) VALUES (?, ?)', (campo1, campo2))
+    conn.commit()
+
+    conn.close()
+
+    return render_template("exito.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
